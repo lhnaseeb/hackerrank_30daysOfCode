03bd06af003f73ba731f3a01135cf4176cd70057
lhnaseeb <46655419+lhnaseeb@users.noreply.github.com>
2020-01-29 11:31:39 -0500

    Day 3: Coin Toss Simulation

---
+++ CoinToss.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class CoinToss {
+
+    public String tossACoin() {
+        Random rand = new Random();
+        int toss =  Math.abs(rand.nextInt()) % 2;
+        if (toss == 1){
+            return  "heads";
+        }
+        else{
+            return  "tails";
+        }
+
+    }
+    public static void main (String[] args) {
+        CoinToss game = new CoinToss();
+        
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+
+    }
+}
